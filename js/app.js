3afdea7a54bd4b5d855322af45952e2efe891a48
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-22 21:10:49 -0700

    Add GifRequest object. Create trendingGIFobj and searchGIFobj. Have those two objects delegate/inherit GifRequest property.

--- js/app.js
+++ js/app.js
@@ -2,22 +2,7 @@
 
 var searchGIF = document.querySelector('#searchGIF');
 
-var searchGIFobj = {
-  searchGIFEvent() {
-    return searchGIF.addEventListener('keyup', this.requestGIF);
-  },
-  requestGIF(e) {
-    if (e.keyCode === 13) {
-      var searchEndpoint = `http://api.giphy.com/v1/gifs/search?q=${this.value}&api_key=dc6zaTOxFJmzC`;
-      searchGIFobj.ajaxReq(searchEndpoint)
-      .then(function(val){
-        console.log(val);
-      })
-      .catch( function(err){
-        console.log(err);
-      } )
-    }
-  },
+var GifRequest = {
   ajaxReq(url) {
     return new Promise( function(resolve, reject){
       var xhr = new XMLHttpRequest();
@@ -35,6 +20,35 @@ var searchGIFobj = {
       }
     } );
   }
+};
+
+var trendingGIFobj = Object.create(GifRequest);
+trendingGIFobj.requestTrendingGifs = function() {
+  var trendingGIFS = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+  this.ajaxReq(trendingGIFS)
+  .then(function(val){
+    console.log(val);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
 }
 
+var searchGIFobj = Object.create(GifRequest);
+searchGIFobj.searchGIFEvent = function() {
+  return searchGIF.addEventListener('keyup', this.requestGIF);
+};
+searchGIFobj.requestGIF = function(e) {
+  if (e.keyCode === 13) {
+    searchGIFobj.ajaxReq(`http://api.giphy.com/v1/gifs/search?q=${this.value}&api_key=dc6zaTOxFJmzC`)
+    .then(function(val){
+      console.log(val);
+    })
+    .catch( function(err){
+      console.log(err);
+    } )
+  }
+}
+
+trendingGIFobj.requestTrendingGifs();
 searchGIFobj.searchGIFEvent();
