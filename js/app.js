741a653b540e790bf715e4ac77f412b2a871b396
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-22 20:18:38 -0700

    Add object to organize functions.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,40 @@
 "use strict"
 
-var searchEndpoint = "http://api.giphy.com/v1/gifs/search?q=psycho&api_key=dc6zaTOxFJmzC";
+var searchGIF = document.querySelector('#searchGIF');
 
-ajaxReq(searchEndpoint)
-  .then(function(val){
-    console.log(val);
-  })
-  .catch( function(err){
-    console.log(err);
-  } )
-
-
-
-function ajaxReq(url) {
-  return new Promise( function(resolve, reject){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = handleData;
-    xhr.send();
-
-    function handleData() {
-      if (xhr.status === 200 && xhr.readyState === 4) {
-        var data = JSON.parse(xhr.responseText);
-        resolve (data);
-      } else {
-        reject (xhr.statusText);
-      }
+var searchGIFobj = {
+  searchGIFEvent() {
+    return searchGIF.addEventListener('keyup', this.requestGIF);
+  },
+  requestGIF(e) {
+    if (e.keyCode === 13) {
+      var searchEndpoint = `http://api.giphy.com/v1/gifs/search?q=${this.value}&api_key=dc6zaTOxFJmzC`;
+      searchGIFobj.ajaxReq(searchEndpoint)
+      .then(function(val){
+        console.log(val);
+      })
+      .catch( function(err){
+        console.log(err);
+      } )
     }
-  } );
+  },
+  ajaxReq(url) {
+    return new Promise( function(resolve, reject){
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = handleData;
+      xhr.send();
+
+      function handleData() {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+          var data = JSON.parse(xhr.responseText);
+          resolve (data);
+        } else {
+          reject (xhr.statusText);
+        }
+      }
+    } );
+  }
 }
+
+searchGIFobj.searchGIFEvent();
